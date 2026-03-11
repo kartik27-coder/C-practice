fd328018d57b5d72aeac80394bfd892ed4755ae7
kartik kashyap <krishnakashyap5b@gmail.com>
2026-03-11 23:29:00 +0530

    Update factorialsOfnnumbers.c
    Added comments to clarify the purpose and functionality of the code.

--- factorialsOfnnumbers.c
+++ factorialsOfnnumbers.c
@@ -1,13 +1,13 @@
+//program to find the factorial on n number
 #include<stdio.h>
 int main (){
     int n;
     printf("Enter the value of n : ");
-    scanf("%d",&n);
+    scanf("%d",&n);            //taking the number 
         int product = 1;
-        for(int i = 1 ; i<=n ; i++){
-        product = product * i;
-        printf("The factorial of %d = %d\n",i,product);
-
+        for(int i = 1 ; i<=n ; i++){                //using a loop
+        product = product * i;                      //calculating the factorial here
+        printf("The factorial of %d = %d\n",i,product);        //output
         }
     return 0;
 }
